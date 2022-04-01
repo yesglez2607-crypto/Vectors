e3350713faddd76d94dc316991fe891f3c1a2dc9
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-04-01 10:56:04 -0700

    d

--- VectorsApp.py
+++ VectorsApp.py
@@ -7,3 +7,8 @@ Ay = st.slider('Magnitud del vector B', 0.0, 10.0, 2.0,step=0.1)
 Bx = st.slider('Dirección del vector A', 0.0, 360.0, 10.0)
 By = st.slider('Dirección del vector B', 0.0, 360.0, 45.0)
 
+x = np.linspace(-10,10,100,dtype=float)
+y = np.sin(x)
+fig, ax = plt.subplots()
+ax.plot(x,y)
+st.pyplot(fig)

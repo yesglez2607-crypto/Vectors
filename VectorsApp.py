66aa84517d6bebe67bd2f4af93ea9253c4e519a1
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-04-01 10:36:00 -0700

    d

--- VectorsApp.py
+++ VectorsApp.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 st.title("App para suma de vectores")
-st.markdown("Elige la magnitud y dirección de los vectores $ \overrightarrow{A} $ y $ \vec{B} $ ")
+st.markdown("Elige la magnitud y dirección de los vectores $ \overrightarrow{A} $ y $ \overrightarrow{B} $ ")

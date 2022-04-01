7fd53938009223cc04615522ce499805ff8fb5af
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-04-01 10:41:48 -0700

    d

--- VectorsApp.py
+++ VectorsApp.py
@@ -4,6 +4,6 @@ st.title("App para suma de vectores")
 st.markdown("Elige la magnitud y dirección de los vectores $ \overrightarrow{A} $ y $ \overrightarrow{B} $ ")
 Ax = st.slider('Magnitud del vector A', 0, 10, 2)
 Ay = st.slider('Magnitud del vector B', 0, 10, 2)
-Bx = st.slider('Dirección del vector A', 0, 360, 10)
-By = st.slider('Dirección del vector B', 0, 360, 45)
+Bx = st.slider('Dirección del vector A', 0, 360, 10,0.05)
+By = st.slider('Dirección del vector B', 0, 360, 45,0.05)
 

400aad718708174517d99b374d7236975b4db97f
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-04-01 12:13:16 -0700

    d

--- VectorsApp.py
+++ VectorsApp.py
@@ -3,10 +3,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 st.title("App para suma de vectores")
 st.markdown("Elige la magnitud y dirección de los vectores $ \overrightarrow{A} $ y $ \overrightarrow{B} $ ")
+
+
 A_mag = st.slider('Magnitud del vector A', 0.0, 10.0, 1.0,step=0.1)
 B_mag = st.slider('Magnitud del vector B', 0.0, 10.0, 2.0,step=0.1)
 A_theta = st.slider('Dirección del vector A', 0.0, 360.0, 10.0)
 B_theta = st.slider('Dirección del vector B', 0.0, 360.0, 45.0)
+
+
 Ax = A_mag*np.cos(2.0*np.pi*A_theta/360.0)
 Ay = A_mag*np.sin(2.0*np.pi*A_theta/360.0)
 Bx = B_mag*np.cos(2.0*np.pi*B_theta/360.0)
@@ -31,11 +35,11 @@ ax.arrow(Ax,Ay,Bx,By,width=abs(xmax)*0.01,color='blue',length_includes_head=True
 ax.arrow(0,0,Cx,Cy,width=abs(xmax)*0.01,color='green',length_includes_head=True)
 ax.grid()
 
-st.write("xmin = ",xmin, " xmax = ",xmax)
-st.write("ymin = ",ymin, " ymax = ",ymax)
-st.write(Ax,Ax+Bx,abs(Ax),abs(Ax+Bx))
-st.write(Ay,Ay+By,abs(Ay),abs(Ay+By))
-st.write(Cx,Cy)
+#st.write("xmin = ",xmin, " xmax = ",xmax)
+#st.write("ymin = ",ymin, " ymax = ",ymax)
+#st.write(Ax,Ax+Bx,abs(Ax),abs(Ax+Bx))
+#st.write(Ay,Ay+By,abs(Ay),abs(Ay+By))
+#st.write(Cx,Cy)
 #xmin, xmax, ymin, ymax = ax.axis([xmin, xmax, ymin, ymax])
 ax.set(xlim=(xmin, xmax), ylim=(ymin, ymax))
 ax.axis('on')

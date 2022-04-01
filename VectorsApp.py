957f496d082045a1496bb8741c42105aaf8ec58f
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-04-01 10:40:44 -0700

    d

--- VectorsApp.py
+++ VectorsApp.py
@@ -2,3 +2,8 @@ import streamlit as st
 import matplotlib.pyplot as plt
 st.title("App para suma de vectores")
 st.markdown("Elige la magnitud y dirección de los vectores $ \overrightarrow{A} $ y $ \overrightarrow{B} $ ")
+Ax = st.slider('Magnitud del vector A', 0, 10, 2)
+Ay = st.slider('Magnitud del vector B', 0, 10, 2)
+Bx = st.slider('Dirección del vector A', 0, 360, 10)
+By = st.slider('Dirección del vector B', 0, 360, 45)
+

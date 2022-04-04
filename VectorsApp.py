ab8c69c6067c47b2bde068d22e0cbf71609b26b1
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-04-04 12:34:09 -0600

    c

--- VectorsApp.py
+++ VectorsApp.py
@@ -38,7 +38,7 @@ head_w = 20.0*width_l
 ax.arrow(0,0,Ax,Ay,width=width_l,color='red',length_includes_head=True,animated=True,head_width=head_w)
 ax.arrow(Ax,Ay,Bx,By,width=width_l,color='blue',length_includes_head=True,animated=True,head_width=head_w)
 ax.arrow(0,0,Cx,Cy,width=width_l*10,color='darkorange',length_includes_head=True,animated=True,head_width=head_w)
-ax.grid(color='green', linestyle='-', linewidth=0.2)
+ax.grid(color='green', linestyle='-', linewidth=0.2,which='major' )
 
 ax.set(xlim=(-le, le), ylim=(-le, le))
 st.pyplot(fig)

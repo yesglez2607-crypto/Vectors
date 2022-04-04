54c05269c054de4a58edaaeb624db7c178b0a40b
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-04-04 11:28:32 -0600

    Update VectorsApp.py

--- VectorsApp.py
+++ VectorsApp.py
@@ -11,24 +11,30 @@ with st.sidebar:
     A_theta = st.slider('Dirección del vector A', 0.0, 360.0, 10.0)
     B_theta = st.slider('Dirección del vector B', 0.0, 360.0, 45.0)
 
+deg2rad = 2.0*np.pi/360.0
+A_theta_rad = deg2rad*A_theta
+B_theta_rad = deg2rad*B_theta
 
-Ax = A_mag*np.cos(2.0*np.pi*A_theta/360.0)
-Ay = A_mag*np.sin(2.0*np.pi*A_theta/360.0)
-Bx = B_mag*np.cos(2.0*np.pi*B_theta/360.0)
-By = B_mag*np.sin(2.0*np.pi*B_theta/360.0)
+    
+Ax = A_mag*np.cos(A_theta_rad)
+Ay = A_mag*np.sin(A_theta_rad)
+Bx = B_mag*np.cos(B_theta_rad)
+By = B_mag*np.sin(B_theta_rad)
 Cx = Ax + Bx
 Cy = Ay + By
 C_mag = np.sqrt(Cx*Cx+Cy*Cy)
+
 #st.write(Ax,Ay)
 #x = np.linspace(-10,10,100,dtype=float)
 #y = np.sin(x)
 #fig, ax = plt.subplots()
+
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 #
-xmax = max(abs(Ax),abs(Ax+Bx))+max(abs(Ax),abs(Ax+Bx))*0.3
+xmax = max(abs(Ax),abs(Cx))+max(abs(Ax),abs(Cx))*0.3
 xmin = -xmax
-ymax = max(abs(Ay),abs(Ay+By))+max(abs(Ay),abs(Ay+By))*0.3
+ymax = max(abs(Ay),abs(Cy))+max(abs(Ay),abs(Cy))*0.3
 ymin = -ymax
 
 

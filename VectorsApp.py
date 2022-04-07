037a8e8f0778bf6151418654d33b6f28f9a39b96
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-04-07 13:06:24 -0600

    Update VectorsApp.py

--- VectorsApp.py
+++ VectorsApp.py
@@ -8,9 +8,12 @@ with st.sidebar:
     st.markdown("Elige la magnitud y dirección de los vectores $ \overrightarrow{A} $ y $ \overrightarrow{B} $ ")
     #A_mag = st.slider('Magnitud del vector A', 0.0, 10.0, 1.0,step=0.1)
     A_mag = st.number_input("pon un número",0)
-    B_mag = st.slider('Magnitud del vector B', 0.0, 10.0, 2.0,step=0.1)
-    A_theta = st.slider('Dirección del vector A', 0.0, 360.0, 10.0)
-    B_theta = st.slider('Dirección del vector B', 0.0, 360.0, 45.0)
+    B_mag = st.number_input("pon un número",0)
+    A_theta = st.number_input("pon un número",0)
+    B_theta= st.number_input("pon un número",0)
+    #B_mag = st.slider('Magnitud del vector B', 0.0, 10.0, 2.0,step=0.1)
+   # A_theta = st.slider('Dirección del vector A', 0.0, 360.0, 10.0)
+  #  B_theta = st.slider('Dirección del vector B', 0.0, 360.0, 45.0)
 
 deg2rad = 2.0*np.pi/360.0
 A_theta_rad = deg2rad*A_theta

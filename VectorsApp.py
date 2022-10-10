53b2b89fde3dbb11340932b970831daf8bbdd2ae
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-10-10 08:43:52 -0600

    .

--- VectorsApp.py
+++ VectorsApp.py
@@ -36,9 +36,7 @@ FRy = F2y + F3y
 FR_mag = np.sqrt(FRx*FRx+FRy*FRy)
 
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='polar')
-ax.set_theta_zero_location('E')
-ax.set_theta_direction(1) # clockwise
+ax = fig.add_subplot(1,1,1)
 
 xm1 = max(abs(F1x),abs(F2x))
 xm2 = max(abs(F3x),abs(FRx))
@@ -51,8 +49,8 @@ width_l = max(xmax,ymax)*0.005
 le1 = max(xmax,ymax)
 le = le1*1.3
 head_w = 20.0*width_l
-#ax.axhline(0,color='black') # x = 0
-#ax.axvline(0,color='black') # y = 0
+ax.axhline(0,color='black') # x = 0
+ax.axvline(0,color='black') # y = 0
 
 
 ax.arrow(0,0,F1x,0.0,width=width_l,color='purple',length_includes_head=True,animated=True,head_width=head_w)
@@ -61,17 +59,16 @@ ax.arrow(0,0,F3x,F3y,width=width_l,color='blue',length_includes_head=True,animat
 ax.arrow(0,0,FRx,FRy,width=width_l,color='darkorange',length_includes_head=True,animated=True,head_width=head_w)
 ax.grid(color='green', linestyle='-.', linewidth=0.2)
 
-#ax.grid(True)
+#ax = plt.subplot(111, projection='polar')
+#ax.plot(azi*np.pi/180, time, color='black', marker='D', markerfacecolor='limegreen')
+#ax.set_theta_zero_location('N')
+#ax.set_theta_direction(-1) # clockwise
+ax.grid(True)
 
 # ax.set_ylabel('Time', color='crimson')
-#ax.tick_params(axis='y', colors='crimson')
+ax.tick_params(axis='y', colors='crimson')
 
-#ax.set(xlim=(-le, le), ylim=(-le, le))
-#time = np.array([4 ,5 ,6, 7, 10])
-#azi = np.array([70 ,100 ,120, 150, 170])
-#ax = fig.add_subplot(111, projection='polar')
-#ax.plot(azi*np.pi/180, color='black', marker='D', markerfacecolor='limegreen')
-
+ax.set(xlim=(-le, le), ylim=(-le, le))
 st.pyplot(fig)
 st.write(" $ \overrightarrow{F_1} = $ ","{:.4f}".format(F1x),"$ i $")
 st.write(" $ \overrightarrow{F_2} = $ ","{:.4f}".format(F2x),"$ i $"," + ","{:.4f}".format(F2y),"$ j $")

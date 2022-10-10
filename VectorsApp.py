a6c556592ed8159a4a17a28b4521af67c33a5455
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-10-10 11:43:55 -0600

    .

--- VectorsApp.py
+++ VectorsApp.py
@@ -51,8 +51,8 @@ width_l = max(xmax,ymax)*0.015
 lx=xmax*1.2
 ly=ymax*1.5
 head_w = 4.0*width_l
-ax.axhline(0,color='black') # x = 0
-ax.axvline(0,color='black') # y = 0
+#ax.axhline(0,color='black') # x = 0
+#ax.axvline(0,color='black') # y = 0
 
 ax.annotate("", xy=(0.0,F1x), xytext=(0, 0),arrowprops=dict(arrowstyle="->",color='red',lw=2.5))
 ax.arrow(0.,0.,F1x,0.0,width=width_l,color='purple',length_includes_head=True,animated=True,head_width=head_w)

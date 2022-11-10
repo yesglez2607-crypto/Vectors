79a4e0c705885eb31469b4b05fa2e8cc40fddd12
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-11-10 13:58:19 -0600

    .

--- VectorsApp.py
+++ VectorsApp.py
@@ -2,6 +2,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 import math as mt
+
 st.set_page_config(layout="wide")
 st.title("Práctica de laboratorio No 5")
 st.markdown(" ##     Vectores: Fuerzas en equilibrio ")

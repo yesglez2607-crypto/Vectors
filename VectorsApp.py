3332f1332e492d1eb1d8478f9c30129b946806be
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-11-10 13:54:44 -0600

    .

--- VectorsApp.py
+++ VectorsApp.py
@@ -77,20 +77,15 @@ str4="#### $ \overrightarrow{F_R} = $ " + " {:.4f}".format(FRx) + " $ i $" + " +
 str5="###  $ \overrightarrow{F_1} + \overrightarrow{F_R} = $" + " {:.4f}".format(FRx+F1x) + " $ i $" + " + " + " {:.4f}".format(FRy) + " $ j $"
 
 with col2:
-    st.write(" ")
-    st.write(" ")
-    st.write(" ")
-    st.write(" ")
-    st.write(" ")
-    st.markdown(""" ### Vectores de las fuerzas 
+    st.markdown(""" 
+    
+    
+    ### Vectores de las fuerzas 
     
 
     
     
     """)
-
-
-
 
     if F1_mag != 0.0:
         st.markdown(str1)

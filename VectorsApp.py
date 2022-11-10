994402919115b3b59c758b3292c82bafcc88bb31
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-11-10 13:30:17 -0600

    .

--- VectorsApp.py
+++ VectorsApp.py
@@ -37,7 +37,7 @@ FR_theta = mt.atan2(FRy,FRx)
 fig = plt.figure()
 ax = fig.add_axes([0.0,0.0,1.0,1.0],polar=True)
 
-plt.rc('grid', color='#316931', linewidth=3, linestyle='-')
+plt.rc('grid', color='#316931', linewidth=6, linestyle='-')
 plt.rc('xtick', labelsize=16)  
 plt.rc('ytick', labelsize=1)
 

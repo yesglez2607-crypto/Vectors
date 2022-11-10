7f0639628e8b558d65eeb835b114c94abde0b816
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-11-10 12:22:53 -0600

    .

--- VectorsApp.py
+++ VectorsApp.py
@@ -50,6 +50,18 @@ ym1 = max(abs(F2y),abs(F3y))
 ymax = max(abs(FRy),ym1)
 width_l = max(xmax,ymax)*0.015
 
+nn1 = max(F1_mag,F2_mag)
+nn1 = max(F3_mag,FR_mag)
+nn3=max(nn1,nn2)
+
+F1_mag=F1_mag/nn3
+F2_mag=F2_mag/nn3
+F3_mag=F3_mag/nn3
+FR_mag=FR_mag/nn3
+width1 = F1_mag
+width2 = F2_mag
+width3 = F3_mag
+width4 = FR_mag
 lx=xmax*1.2
 ly=ymax*1.5
 head_w = 4.0*width_l
@@ -57,14 +69,14 @@ col1, col2 = st.columns(2)
 
 with col1:
     if F1_mag != 0.0:
-        ax.arrow(0.0,0.0,0.0,F1x,width=width_l,color='purple',length_includes_head=True,animated=True,head_width=head_w)
+        ax.arrow(0.0,0.0,0.0,F1x,width=width1,color='purple',length_includes_head=True,animated=True,head_width=head_w)
        # st.write(" $ \overrightarrow{F_1} = $ ","{:.4f}".format(F1x),"$ i $")
     if F2_mag !=0.0 and F2_theta != 0.0:    
-        ax.arrow(F2_theta*np.pi/180.0,0.0,0.0,F2_mag,width=width_l,color='red',length_includes_head=True,animated=True,head_width=head_w)
+        ax.arrow(F2_theta*np.pi/180.0,0.0,0.0,F2_mag,width=width2,color='red',length_includes_head=True,animated=True,head_width=head_w)
        # st.write(" $ \overrightarrow{F_2} = $ ","{:.4f}".format(F2x),"$ i $"," + ","{:.4f}".format(F2y),"$ j $")
     if F3_mag !=0.0 and F3_theta != 0.0:
-        ax.arrow(F3_theta*np.pi/180.0,0.0,0.0,F3_mag,width=width_l,color='blue',length_includes_head=True,animated=True,head_width=head_w)
-        ax.arrow(FR_theta,0.0,0.0,FR_mag,width=width_l,color='darkorange',length_includes_head=True,animated=True,head_width=head_w) 
+        ax.arrow(F3_theta*np.pi/180.0,0.0,0.0,F3_mag,width=width3,color='blue',length_includes_head=True,animated=True,head_width=head_w)
+        ax.arrow(FR_theta,0.0,0.0,FR_mag,width=width4,color='darkorange',length_includes_head=True,animated=True,head_width=head_w) 
        # st.write(" $ \overrightarrow{F_3} = $ ","{:.4f}".format(F3x),"$ i $"," + ","{:.4f}".format(F3y),"$ j $")
        # st.write(" $ \overrightarrow{F_R} = $ ","{:.4f}".format(F3y),"$ i $"," + ","{:.4f}".format(FRy),"$ j $")
        # st.write(" $ \overrightarrow{F_1} + \overrightarrow{F_R} = $","{:.4f}".format(FRx+F1x),"$ i $"," + ","{:.4f}".format(FRy),"$ j $")

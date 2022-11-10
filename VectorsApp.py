9f028c17d3d5d01adf02bb73650a0cb74b7fe5ca
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-11-10 16:29:39 -0600

    .

--- VectorsApp.py
+++ VectorsApp.py
@@ -22,6 +22,10 @@ with st.sidebar:
     
 
     st.image('Dorado.jpg')
+    st.markdown(""" #### Autores:
+             * Dr. Juan Pedro Palomares Báez
+             * Dr. José Manuel Nápoles Duarte
+             * MC. Carlos Armando de la Vega Cobos""")
 
 
 deg2rad = 2.0*np.pi/360.0

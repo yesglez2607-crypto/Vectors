7ab0234ec260bcc64177125e7e458c6b8ea7d372
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-11-10 10:45:17 -0600

    .

--- VectorsApp.py
+++ VectorsApp.py
@@ -55,21 +55,19 @@ ly=ymax*1.5
 head_w = 4.0*width_l
 col1, col2 = st.columns(2)
 
-
-if F1_mag != 0.0:
-    with col2:
+with col1:
+    if F1_mag != 0.0:
         ax.arrow(0.0,0.0,0.0,F1x,width=width_l,color='purple',length_includes_head=True,animated=True,head_width=head_w)
-    with col1:
         st.write(" $ \overrightarrow{F_1} = $ ","{:.4f}".format(F1x),"$ i $")
-if F2_mag !=0.0 and F2_theta != 0.0:    
-    ax.arrow(F2_theta*np.pi/180.0,0.0,0.0,F2_mag,width=width_l,color='red',length_includes_head=True,animated=True,head_width=head_w)
-    st.write(" $ \overrightarrow{F_2} = $ ","{:.4f}".format(F2x),"$ i $"," + ","{:.4f}".format(F2y),"$ j $")
-if F3_mag !=0.0 and F3_theta != 0.0:
-    ax.arrow(F3_theta*np.pi/180.0,0.0,0.0,F3_mag,width=width_l,color='blue',length_includes_head=True,animated=True,head_width=head_w)
-    ax.arrow(FR_theta,0.0,0.0,FR_mag,width=width_l,color='darkorange',length_includes_head=True,animated=True,head_width=head_w) 
-    st.write(" $ \overrightarrow{F_3} = $ ","{:.4f}".format(F3x),"$ i $"," + ","{:.4f}".format(F3y),"$ j $")
-    st.write(" $ \overrightarrow{F_R} = $ ","{:.4f}".format(F3y),"$ i $"," + ","{:.4f}".format(FRy),"$ j $")
-    st.write(" $ \overrightarrow{F_1} + \overrightarrow{F_R} = $","{:.4f}".format(FRx+F1x),"$ i $"," + ","{:.4f}".format(FRy),"$ j $")
+    if F2_mag !=0.0 and F2_theta != 0.0:    
+        ax.arrow(F2_theta*np.pi/180.0,0.0,0.0,F2_mag,width=width_l,color='red',length_includes_head=True,animated=True,head_width=head_w)
+        st.write(" $ \overrightarrow{F_2} = $ ","{:.4f}".format(F2x),"$ i $"," + ","{:.4f}".format(F2y),"$ j $")
+    if F3_mag !=0.0 and F3_theta != 0.0:
+        ax.arrow(F3_theta*np.pi/180.0,0.0,0.0,F3_mag,width=width_l,color='blue',length_includes_head=True,animated=True,head_width=head_w)
+        ax.arrow(FR_theta,0.0,0.0,FR_mag,width=width_l,color='darkorange',length_includes_head=True,animated=True,head_width=head_w) 
+        st.write(" $ \overrightarrow{F_3} = $ ","{:.4f}".format(F3x),"$ i $"," + ","{:.4f}".format(F3y),"$ j $")
+        st.write(" $ \overrightarrow{F_R} = $ ","{:.4f}".format(F3y),"$ i $"," + ","{:.4f}".format(FRy),"$ j $")
+        st.write(" $ \overrightarrow{F_1} + \overrightarrow{F_R} = $","{:.4f}".format(FRx+F1x),"$ i $"," + ","{:.4f}".format(FRy),"$ j $")
 
-ax.grid(True)
-st.pyplot(fig)
+    ax.grid(True)
+    st.pyplot(fig)

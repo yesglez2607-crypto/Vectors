6cb32bcee6a721d8c578694041afb5f8101d8a07
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-11-10 13:19:00 -0600

    .

--- VectorsApp.py
+++ VectorsApp.py
@@ -19,7 +19,6 @@ with st.sidebar:
     F3_theta = st.number_input("ángulo de  F3:",0.0000)
 
 
-
 deg2rad = 2.0*np.pi/360.0
 F2_theta_rad = deg2rad*F2_theta
 F3_theta_rad = deg2rad*F3_theta
@@ -36,19 +35,11 @@ FR_mag = np.sqrt(FRx*FRx+FRy*FRy)
 FR_theta = mt.atan2(FRy,FRx)
 
 fig = plt.figure()
-
 ax = fig.add_axes([0.0,0.0,1.0,1.0],polar=True)
 
 plt.rc('grid', color='#316931', linewidth=3, linestyle='-')
 plt.rc('xtick', labelsize=16)  
 plt.rc('ytick', labelsize=1)
-
-xm1 = max(abs(F1x),abs(F2x))
-xm2 = max(abs(F3x),abs(FRx))
-xmax = max(xm1,xm2)
-ym1 = max(abs(F2y),abs(F3y))
-ymax = max(abs(FRy),ym1)
-width_l = max(xmax,ymax)*0.015
 
 nn1 = max(F1_mag,F2_mag)
 nn2 = max(F3_mag,FR_mag)
@@ -58,32 +49,20 @@ F1_mag=F1_mag/nn3
 F2_mag=F2_mag/nn3
 F3_mag=F3_mag/nn3
 FR_mag=FR_mag/nn3
-nn3=1.0 
+
 width1 = 0.2 
+head_w=width1*1.5
 
-head_w1=width1*1.5
-head_w2=width1*1.5
-head_w3=width1*1.5
-head_w4=width1*1.5
-
-lx=xmax*1.2
-ly=ymax*1.5
-head_w = 4.0*width_l
 col1, col2 = st.columns(2)
 
 with col1:
     if F1_mag != 0.0:
         ax.arrow(0.0,0.0,0.0,F1_mag,width=width1,edgecolor = 'black', facecolor = 'purple',length_includes_head=True,animated=True,head_width=head_w1,lw=2.0)
-       # st.write(" $ \overrightarrow{F_1} = $ ","{:.4f}".format(F1x),"$ i $")
     if F2_mag !=0.0 and F2_theta != 0.0:    
         ax.arrow(F2_theta*np.pi/180.0,0.0,0.0,F2_mag,width=width1,edgecolor = 'black', facecolor = 'red',length_includes_head=True,animated=True,head_width=head_w2,lw=2.0)
-       # st.write(" $ \overrightarrow{F_2} = $ ","{:.4f}".format(F2x),"$ i $"," + ","{:.4f}".format(F2y),"$ j $")
     if F3_mag !=0.0 and F3_theta != 0.0:
         ax.arrow(F3_theta*np.pi/180.0,0.0,0.0,F3_mag,width=width1,edgecolor = 'black', facecolor = 'blue',length_includes_head=True,animated=True,head_width=head_w3,lw=2.0)
         ax.arrow(FR_theta,0.0,0.0,FR_mag,width=width1,edgecolor = 'black', facecolor = 'darkorange',length_includes_head=True,animated=True,head_width=head_w4,lw=2.0) 
-       # st.write(" $ \overrightarrow{F_3} = $ ","{:.4f}".format(F3x),"$ i $"," + ","{:.4f}".format(F3y),"$ j $")
-       # st.write(" $ \overrightarrow{F_R} = $ ","{:.4f}".format(F3y),"$ i $"," + ","{:.4f}".format(FRy),"$ j $")
-       # st.write(" $ \overrightarrow{F_1} + \overrightarrow{F_R} = $","{:.4f}".format(FRx+F1x),"$ i $"," + ","{:.4f}".format(FRy),"$ j $")
 
     ax.grid(True,lw=0.8,linestyle='--')
     st.pyplot(fig)

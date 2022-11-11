744ce6581cefb9d1380a3776d02316429a7296cc
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-11-10 21:31:01 -0600

    .

--- VectorsApp.py
+++ VectorsApp.py
@@ -115,8 +115,9 @@ with col2:
 
     if F1_mag != 0.0:
         str1="#### $ \overrightarrow{F_1} = $ " + " {:.4f}".format(F1x) + " $ i $   </font>"
-        strr = '<p style="font-family:sans-serif; color:Green; font-size: 42px;">' + str1 + '</p>'
-        st.markdown(strr, unsafe_allow_html=True)
+        strr = st.markdown(str1)
+        strrr = '<p style="font-family:sans-serif; color:Green; font-size: 42px;">' + strr + '</p>'
+        st.markdown(strrr, unsafe_allow_html=True)
     if F2_mag !=0.0 and F2_theta != 0.0: 
         st.markdown(str2)   
     if F3_mag !=0.0 and F3_theta != 0.0:

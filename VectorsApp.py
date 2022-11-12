e5b18096f5aece1984752e048fce9f49645f012a
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-11-11 22:24:31 -0600

    .

--- VectorsApp.py
+++ VectorsApp.py
@@ -90,6 +90,9 @@ with col1:
 
 
     st.pyplot(fig)
+    file = None
+    if btn == True:
+        file=plt.savefig('imagen.png')
 
     btn = st.download_button(
             label="Guardar imagen",
@@ -97,8 +100,7 @@ with col1:
             file_name="imagen.png",
             mime="image/png"
           )
-    if btn == True:
-        file=plt.savefig('imagen.png')
+
 
 
 str1="#### $ \overrightarrow{F_1} = $ " + " {:.4f}".format(F1x) + " $ i $"

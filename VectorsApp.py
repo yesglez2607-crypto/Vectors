4d4b11cefc5686c7c13a9e5d57433318ea8b013a
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-11-12 13:16:44 -0600

    .

--- VectorsApp.py
+++ VectorsApp.py
@@ -55,7 +55,7 @@ FR_mag = np.sqrt(FRx*FRx+FRy*FRy)
 FR_theta = mt.atan2(FRy,FRx)
 
 fig = plt.figure(figsize=(6, 5), dpi=300)
-ax = fig.add_axes([0.01,0.025,0.8,0.8],polar=True)
+ax = fig.add_axes([0.01,0.05,0.8,0.8],polar=True)
 
 plt.rc('grid', color='#316931', linewidth=2, linestyle='-')
 plt.rc('xtick', labelsize=12)  

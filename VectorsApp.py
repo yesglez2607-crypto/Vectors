0c159f5ea4b180fc31e099608de3a10253e4840f
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-11-12 11:06:55 -0600

    .

--- VectorsApp.py
+++ VectorsApp.py
@@ -90,7 +90,7 @@ ax.grid(True,lw=0.8,linestyle='--',zorder=0)
 with col1:
     if F1_mag != 0.0:
         ax.arrow(0.0,0.0,0.0,F1_mag,width=width1,edgecolor = 'black', facecolor = 'darkgreen',length_includes_head=True,animated=True,head_width=head_w,head_length=head_l1,lw=2.0,zorder=3)
-        ax.text(-1.7,0.0,r'$ \overrightarrow{F_1} $',fontsize = 25.0, color='darkgreen',fontstyle='oblique',fontfamily='fantasy')  
+        ax.text(-1.7,-1.0,r'$ \overrightarrow{F_1} $',fontsize = 25.0, color='darkgreen',fontstyle='oblique',fontfamily='fantasy')  
     if F2_mag !=0.0 and F2_theta != 0.0:    
         ax.arrow(F2_theta*np.pi/180.0,0.0,0.0,F2_mag,width=width1,edgecolor = 'black', facecolor = 'red',length_includes_head=True,animated=True,head_width=head_w,head_length=head_l2,lw=2.0,zorder=4)
         ax.text(F2x/nn3,F2y,r'$ \overrightarrow{F_2} $',fontsize = 25.0, color='black',fontstyle='oblique',fontfamily='fantasy') 
